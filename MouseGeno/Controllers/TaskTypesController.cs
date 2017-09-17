44e3a5795f1f842a2601e74b5942cc51899ce2c2
arock83 <arock83@gmail.com>
2017-09-17 18:38:15 -0500

    Blocked on Adding Multiple Tasks

--- MouseGeno/Controllers/TaskTypesController.cs
+++ MouseGeno/Controllers/TaskTypesController.cs
@@ -7,17 +7,23 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using MouseGeno.Data;
 using MouseGeno.Models;
+using MouseGeno.Models.ViewModels;
+using Microsoft.AspNetCore.Identity;
 
 namespace MouseGeno.Controllers
 {
     public class TaskTypesController : Controller
     {
         private readonly ApplicationDbContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
 
-        public TaskTypesController(ApplicationDbContext context)
+        public TaskTypesController(ApplicationDbContext context, UserManager<ApplicationUser> userManager)
         {
             _context = context;
+            _userManager = userManager;
         }
+
+        private Task<ApplicationUser> GetCurrentUserAsync() => _userManager.GetUserAsync(HttpContext.User);
 
         // GET: TaskTypes
         public async Task<IActionResult> Index()
@@ -148,6 +154,70 @@ namespace MouseGeno.Controllers
         private bool TaskTypeExists(int id)
         {
             return _context.TaskType.Any(e => e.TaskTypeID == id);
+        }
+
+        public IActionResult TaskOnCage (int cageID, int taskID)
+        {
+
+            Cage cage = _context.Cage.Single(c => c.CageID == cageID);
+
+            List<Mouse> miceInCage = (
+                from m in _context.Mouse
+                from mc in _context.MouseCage
+                where m.MouseID == mc.MouseID
+                && mc.EndDate == null
+                && mc.CageID == cageID
+                select m).Include(m => m.PK1).Include(m => m.PK2).Include(m => m.Line).ToList();
+
+            foreach (Mouse mouse in miceInCage)
+            {
+                if (mouse.DadID != null)
+                {
+                    mouse.Dad = _context.Mouse.Single(m => m.MouseID == mouse.DadID);
+                }
+                if (mouse.MomID != null)
+                {
+                    mouse.Mom = _context.Mouse.Single(m => m.MouseID == mouse.MomID);
+                }
+            }
+
+
+            TaskOnCageViewModel model = new TaskOnCageViewModel
+            {
+                MiceInCage = miceInCage,
+                TaskType = _context.TaskType.Single(t => t.TaskTypeID == taskID),
+                Cage = cage
+
+            };
+
+
+            return View(model);
+        }
+
+        public async Task<IActionResult> PerformTaskOnCage (List<Mouse> mice, List<MouseTask> mouseTasks, DateTime date, int cageID, int taskTypeID)
+        {
+            ApplicationUser user = await GetCurrentUserAsync();
+
+            foreach (MouseTask mt in mouseTasks)
+            {
+                if(mt.Data != null)
+                {
+                    _context.MouseTask.Add(
+                        new MouseTask
+                        {
+                            MouseID = mt.MouseID,
+                            TaskTypeID = mt.TaskTypeID,
+                            Date = date,
+                            Data = mt.Data,
+                            User = user,
+                            Comments = mt.Comments
+                    }
+                    );
+                }
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToAction("Details", "Cages", new { id = cageID });
         }
     }
 }
